6b0e64acff83480f2117142a5f03ebb196d7ea2e
Lucian D. Crainic <crainic.lucian@gmail.com>
2022-11-17 00:31:24 +0100

    Fixed security issue and Add description methods

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -9,31 +9,18 @@ func (rt *_router) Handler() http.Handler {
 	// Login action
 	rt.router.POST("/", rt.session)
 	// Session User actions
-	rt.router.PUT("/setUsername")
-	rt.router.GET("/steam")
+
 	// User actions
-	rt.router.GET("/profile")
+
 	// User Ban actions
-	rt.router.POST("/ban")
-	rt.router.GET("/ban")
-	rt.router.DELETE("/ban")
+
 	// User Follow actions
-	rt.router.POST("/follow")
-	rt.router.GET("/follow")
-	rt.router.DELETE("/follow")
+
 	// Photo actions
-	rt.router.POST("/photo")
-	rt.router.GET("/photo")
-	rt.router.DELETE("/photo")
+
 	// Like actions
-	rt.router.POST("/like")
-	rt.router.GET("/like")
-	rt.router.DELETE("/like")
+
 	// Comment actions
-	rt.router.POST("/comment")
-	rt.router.GET("/comment")
-	rt.router.PUT("/comment")
-	rt.router.DELETE("/comment")
 
 	return rt.router
 }
--- service/api/likePhoto.go
+++ service/api/likePhoto.go
@@ -7,7 +7,7 @@ import (
 )
 
 // getHelloWorld is an example of HTTP endpoint that returns "Hello world!" as a plain text
-func (rt *_router) session(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (rt *_router) likePhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("content-type", "text/plain")
 	_, _ = w.Write([]byte("Hello World!"))
 }
