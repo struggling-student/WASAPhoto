a9b81d758b423883b14f0d4bf0b56faafa239451
Lucian D. Crainic <crainic.lucian@gmail.com>
2022-11-23 17:51:43 +0100

    Add api handlers

---
+++ service/api/add-comment.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// getHelloWorld is an example of HTTP endpoint that returns "Hello world!" as a plain text
+func (rt *_router) commentPhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("content-type", "text/plain")
+	_, _ = w.Write([]byte("Hello World!"))
+}
--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -6,21 +6,31 @@ import (
 
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
-	// Login action
-	rt.router.POST("/", rt.session)
-	// Session User actions
-
-	// User actions
-
-	// User Ban actions
-
-	// User Follow actions
-
-	// Photo actions
-
-	// Like actions
-
-	// Comment actions
-
+	//? Login - Functions for logging in
+	rt.router.POST("/session", rt.session)
+	//TODO User - Functions for user actions
+	rt.router.PUT("/user/:username/setusername", rt.setMyUserName)
+	rt.router.GET("/user/:username/stream", rt.getMyStream)
+	rt.router.GET("/user/:username/profile", rt.getUserProfile)
+	//? Photo - Functions for photo actions
+	rt.router.PUT("/users/:username/photo/:photoid", rt.banUser)
+	rt.router.DELETE("/users/:username/photo/:photoid", rt.unbanUser)
+	rt.router.GET("/users/:username/photo", rt.getBans)
+	//? Ban - Functions for ban actions
+	rt.router.PUT("/users/:username/ban/:banid", rt.followUser)
+	rt.router.DELETE("/users/:username/ban/:banid", rt.unfollowUser)
+	rt.router.GET("/users/:username/ban", rt.getFollowers)
+	//? Follow - Functions for follow actions
+	rt.router.PUT("/users/:username/follow/:followid", rt.uploadPhoto)
+	rt.router.DELETE("/users/:username/follow/:followid", rt.deletePhoto)
+	rt.router.GET("/users/:username/follow", rt.getUserPhotos)
+	//? Like - Functions for like actions
+	rt.router.PUT("/users/:username/photo/:photoid/like/:likeid", rt.likePhoto)
+	rt.router.DELETE("/users/:username/photo/:photoid/like/:likeid", rt.unlikePhoto)
+	rt.router.GET("/users/:username/photo/:photoid/like", rt.getLikes)
+	//? Comment - Functions for comment actions
+	rt.router.PUT("/users/:username/photo/:photoid/comment/:commentid", rt.commentPhoto)
+	rt.router.DELETE("/users/:username/photo/:photoid/comment/:commentid", rt.uncommentPhoto)
+	rt.router.GET("/users/:username/photo/:photoid/comment", rt.getComments)
 	return rt.router
 }
