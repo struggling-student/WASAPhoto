85ae75959f65ac5272b251272c1b633933f28174
Lucian D. Crainic <crainic.lucian@gmail.com>
2023-01-02 14:17:18 +0100

    Fixed docker backend and load-config file errors

--- cmd/webapi/load-configuration.go
+++ cmd/webapi/load-configuration.go
@@ -26,7 +26,7 @@ type WebAPIConfiguration struct {
 	}
 	Debug bool
 	DB    struct {
-		Filename string `conf:"default:./service/database/decaf.db"`
+		Filename string `conf:"default:/tmp/decaf.db"`
 	}
 }
 
